5e581eeff138ba966bea05a86cd8d620e25fac5b
ogennaisrael01 <ogennaisrael98@gmail.com>
2025-09-06 04:10:46 +0100

    updated my send mail settings

--- contact/apps.py
+++ contact/apps.py
@@ -1,17 +1,9 @@
-import os
+
 from django.apps import AppConfig
-from django.contrib.auth import get_user_model
 
 
 class ContactConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'contact'
 
-    def ready(self):
-        User = get_user_model()
-        if not User.objects.filter(is_superuser=True).exists():
-            User.objects.create_superuser(
-                username=os.environ.get("DJANGO_SUPERUSER_USERNAME"),
-                email=os.environ.get("DJANGO_SUPERUSER_EMAIL"),
-                password=os.environ.get("DJANGO_SUPERUSER_PASSWORD")
-            )
+    
