285b9df39d4ebe68398e7691af0d6369df0488f1
ogennaisrael01 <ogennaisrael98@gmail.com>
2025-09-12 15:56:32 +0100

    Refactor portfolio templates and styles for improved accessibility and responsiveness
    - Updated base.html to enhance semantic structure and accessibility features.
    - Modified home.html to include a more engaging hero section with improved layout.
    - Revamped projects.html to streamline project display and enhance visual appeal.
    - Added widget_tweaks to settings.py for better form handling.
    - Refined style.css to implement a mobile-first design approach and improve overall aesthetics.
    - Enhanced script.js for better mobile navigation handling and typing effect functionality.

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
-python manage.py migrate --noinput
+python manage.py migrate auth --fake-initial
+python manage.py migrate
 
 python manage.py createsuperuser --noinput || true
 
