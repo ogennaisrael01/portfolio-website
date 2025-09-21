9409278476d38dbcaf4d3b2b15f24a242395c46b
ogennaisrael01 <ogennaisrael98@gmail.com>
2025-09-21 13:39:08 +0100

    Update database configuration and adjust media root path; add new profile image

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -82,10 +82,12 @@ WSGI_APPLICATION = 'portfolio_site.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
